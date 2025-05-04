e7fd4df4ba551975eb310c03168c263ad0722d10
Anderson Souza <andersonpds14@gmail.com>
2025-05-04 16:05:46 -0300

    chore: add commitlint configuration and update lefthook commands to use yarn

---
+++ commitlint.config.ts
@@ -0,0 +1,15 @@
+import type { UserConfig } from '@commitlint/types';
+import { RuleConfigSeverity } from '@commitlint/types';
+
+const Configuration: UserConfig = {
+  extends: ['@commitlint/config-conventional'],
+  parserPreset: 'conventional-changelog-atom',
+  formatter: '@commitlint/format',
+  rules: {
+    'footer-empty': [RuleConfigSeverity.Error, 'never'],
+    'scope-empty': [RuleConfigSeverity.Error, 'never'],
+  },
+  // ...
+};
+
+export default Configuration;
