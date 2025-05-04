4d5ee3cdc41b1e04f5b380d64cba4cea6e31e7ee
Anderson Souza <andersonpds14@gmail.com>
2025-05-04 00:11:56 -0300

    feat(camera): implement camera device extraction hook

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,36 @@
-export function multiply(a: number, b: number): number {
-  return a * b;
-}
+import { useEffect } from 'react';
+import { useMMKV } from 'react-native-mmkv';
+import { Camera } from 'react-native-vision-camera';
+
+export const useExtractCameraDevices = () => {
+  const storage = useMMKV();
+  useEffect(() => {
+    const runOnceKey = 'RN_camera_devices_extracted';
+    const cameraDevicesKey = 'RN_camera_devices';
+
+    const extractDevices = async () => {
+      const hasRunBefore = storage.getBoolean(runOnceKey);
+
+      if (hasRunBefore) {
+        console.debug('Devices information has already been extracted.');
+        return;
+      }
+
+      try {
+        // Extract devices information
+        const devices = Camera.getAvailableCameraDevices();
+        console.log('Extracted devices:', devices);
+
+        // Store devices information in MMKV
+        storage.set(cameraDevicesKey, JSON.stringify(devices));
+
+        // Mark as run
+        storage.set(runOnceKey, true);
+      } catch (error) {
+        console.error('Error extracting devices:', error);
+      }
+    };
+
+    extractDevices();
+  }, [storage]);
+};
