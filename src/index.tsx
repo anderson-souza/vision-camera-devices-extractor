0cc022410a894eaf1ca34f812b0acdf20e188836
Anderson Souza <andersonpds14@gmail.com>
2025-05-04 16:04:50 -0300

    refactor(camera): improve camera devices extraction logic and logging

--- src/index.tsx
+++ src/index.tsx
@@ -5,27 +5,29 @@ import { Camera } from 'react-native-vision-camera';
 export const useExtractCameraDevices = () => {
   const storage = useMMKV();
   useEffect(() => {
-    const runOnceKey = 'RN_camera_devices_extracted';
     const cameraDevicesKey = 'RN_camera_devices';
 
     const extractDevices = async () => {
-      const hasRunBefore = storage.getBoolean(runOnceKey);
+      const hasRunBefore = storage.contains(cameraDevicesKey);
 
       if (hasRunBefore) {
         console.debug('Devices information has already been extracted.');
+        console.debug(
+          'Devices information:',
+          storage.getString(cameraDevicesKey)
+        );
         return;
       }
 
       try {
         // Extract devices information
         const devices = Camera.getAvailableCameraDevices();
-        console.log('Extracted devices:', devices);
+
+        const devicesString = JSON.stringify(devices);
+        console.debug('Devices information:', devicesString);
 
         // Store devices information in MMKV
-        storage.set(cameraDevicesKey, JSON.stringify(devices));
-
-        // Mark as run
-        storage.set(runOnceKey, true);
+        storage.set(cameraDevicesKey, devicesString);
       } catch (error) {
         console.error('Error extracting devices:', error);
       }
